6c3781fd18982f32fa55a0c503ce0e5fe096489e
swhyeon98 <smreo75@gmail.com>
2026-08-25 14:26:48 +0900

    fix: anchack-navy 배포 도메인을 CORS 허용 목록에 추가

--- src/main/java/com/kbait/anchack/common/config/WebConfig.java
+++ src/main/java/com/kbait/anchack/common/config/WebConfig.java
@@ -62,7 +62,8 @@ public class WebConfig
         configuration.setAllowedOrigins(
             Arrays.asList(
                 "http://localhost:5173",
-                "http://127.0.0.1:5173"
+                "http://127.0.0.1:5173",
+                "https://anchack-navy.vercel.app"
             )
         );
 
